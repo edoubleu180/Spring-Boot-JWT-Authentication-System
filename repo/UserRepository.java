c1437b3b996aaab2ea1e27f9167e63dd3e2e0d97
edoubleu180 <elyce18monet@gmail.com>
2026-05-27 19:32:20 -0400

    Create UserRepository interface for user entity

---
+++ repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.jwtauth.repo;
+
+import com.example.jwtauth.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
